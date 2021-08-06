e6a875f9f74623d645fe11917ee825bb640169c7
Brando Collins <ghost.rkrv3@gmail.com>
2021-08-06 09:53:09 -0500

    Completion of Phase One

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import logo from "./logo.svg";
+import {Component} from "react";
 import "./App.css";
+
+
+
+
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    let first = [];
+
+    let numBoxes = 24;
+    
+    for (let i = 0; i <= numBoxes; i++) 
+{
+ first.push({
+  id: i,
+  color: `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+}) 
+}
     // set default state
 
+this.state = { boxes: first}
+
+
+
     // bind methods to this
+
+   this.handleBoxClick = this.handleBoxClick.bind(this);
+
   }
 
+  getRandomColor() {
+    return Math.round(Math.random() * 255) }
+    
+    handleBoxClick(event) {
+      let newBoxes = this.state.boxes.map((box) =>{
+        if (box.id == event.target.id) {
+          box.color = this.getRandomColor()
+        }
+   return box 
+      })
+      this.setState({boxes: newBoxes})
+    }
+
   render() {
+    
     return (
       <main
         style={{
@@ -21,7 +57,14 @@ class App extends Component {
         }}
       >
         <h1>React: State and Props</h1>
-        <div className="App">{/* render boxes */}</div>
+        <div className="App">
+          
+          { 
+          this.state.boxes.map((box) => {
+            return <button id={box.id} key={box.id} onClick={this.handleBoxClick}> {box.color}</button>
+          }) 
+          
+          }</div>
       </main>
     );
   }
